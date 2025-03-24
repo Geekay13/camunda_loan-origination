414cd3f840929120064033a0053d14d2ffdfcf5f
Ganeshkumar.rv <ganeshkumar.rv@changepond.com>
2025-03-24 19:02:52 +0530

    db config created

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const express = require("express");
-const mongoose = require("mongoose");
-require("dotenv").config();
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+// Load environment variables
+dotenv.config();
 
 // Initialize Express app
 const app = express();
 app.use(express.json());
 
-// MongoDB Connection
-mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("MongoDB Connected"))
-  .catch(err => console.error("MongoDB Connection Error:", err));
+// Connect to MongoDB
+connectDB();
 
 // Start the server
 const PORT = process.env.PORT || 3000;
